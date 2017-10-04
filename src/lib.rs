f91f5e5d1fbd5e066304a7f2c07a435ae17caaac
David Björklund <david.bjorklund@gmail.com>
2017-10-04 11:44:58 +0200

    Make the module work

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,33 @@
+extern crate leap_year;
+
+use leap_year::is_leap_year;
+
+pub fn days_in_month(year: i32, month: i32) -> i32 {
+    return match month {
+        2 => if is_leap_year(year) { 29 } else { 28 },
+        4 | 6 | 9 | 11 => 30,
+        _ => 31,
+    };
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn test_days_in_month() {
+        assert_eq!(days_in_month(0, 1), 31);
+        assert_eq!(days_in_month(2000, 2), 29);
+        assert_eq!(days_in_month(2001, 2), 28);
+        assert_eq!(days_in_month(0, 3), 31);
+        assert_eq!(days_in_month(0, 4), 30);
+        assert_eq!(days_in_month(0, 5), 31);
+        assert_eq!(days_in_month(0, 6), 30);
+        assert_eq!(days_in_month(0, 7), 31);
+        assert_eq!(days_in_month(0, 8), 31);
+        assert_eq!(days_in_month(0, 9), 30);
+        assert_eq!(days_in_month(0, 10), 31);
+        assert_eq!(days_in_month(0, 11), 30);
+        assert_eq!(days_in_month(0, 12), 31);
     }
 }
